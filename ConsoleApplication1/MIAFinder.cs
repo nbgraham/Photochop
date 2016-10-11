179bdcb29d119129d8701c311fcda41974ee30b4
tariqbroadnax <tariqbroadnax@gmail.com>
2016-10-11 00:52:43 -0500

    Add files via upload

---
+++ ConsoleApplication1/MIAFinder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    // this is the building block of our 2 algorithms
+  
+    interface MIAFinder
+    {
+       Rectangle mostInterestingArea(Image img);   
+    }
+}
---
+++ ConsoleApplication1/Session.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace ConsoleApplication1
+{
+    class Session
+    {
+        private readonly static TimeSpan MAX_SESSION_DURATION = TimeSpan.FromMinutes(10);
+
+
+        private string sessionID;
+
+        private DateTime start;
+
+        private List<Rectangle> mostInterestingAreas;
+
+        public Session(string sessionID,
+                       Collection<MIAResource> resources)
+        {
+            this.sessionID = sessionID;
+
+            start = DateTime.Now;
+
+            mostInterestingAreas = new List<Rectangle>();
+
+            foreach (MIAResource resource in resources)
+            {
+                Rectangle mostInterestingArea = 
+                    resource.mostInterestingArea();
+
+                mostInterestingAreas.Add(mostInterestingArea);
+            }
+        }
+
+        public Boolean expired()
+        {
+            DateTime now = DateTime.Now;
+
+            if (now - start > MAX_SESSION_DURATION)
+                return true;
+
+            return false;
+        }
+
+        public List<Rectangle> MostInterestingAreas
+        {
+            get;
+        }
+
+    }
+}
