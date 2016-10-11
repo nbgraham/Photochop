298c3b23618938bf7ac04d95b6944c62593b44a3
Simon Boerwinkle <sboerwinkle@gmail.com>
2016-10-11 18:24:05 -0500

    Upload size limit?

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -13,7 +13,12 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             var host = new WebServiceHost(typeof(MyService), new Uri("http://localhost:80/"));
-            var endpoint = host.AddServiceEndpoint(typeof(IMyService), new WebHttpBinding(), "");
+            WebHttpBinding bind = new WebHttpBinding();
+            //MaxBufferSize and TransferMode pulled from http://stackoverflow.com/questions/1354749/wcf-service-to-accept-a-post-encoded-multipart-form-data
+            bind.MaxBufferSize = 65536;
+            bind.TransferMode = TransferMode.Streamed;
+            bind.MaxReceivedMessageSize = 10 * 1024 * 1024;
+            host.AddServiceEndpoint(typeof(IMyService), bind, "");
             host.Open();
             Console.WriteLine("Return to quit...");
             Console.ReadLine();
